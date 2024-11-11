b8b1ca6d79e87f139064cd9ad28ffcb5fe6ada23
Ruchika <ruchikathakur2112@gmal.com>
2024-11-11 14:20:33 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -2,6 +2,21 @@ const decodeTheRing = function (s, p) {
 
     // write your code here
 
+    const decodeTheRing = function (s, p) {
+      // Convert the pattern to a valid regular expression
+      // Replace '*' with '.*' and '?' with '.'
+      const regexPattern = '^' + p.split('*').join('.*').split('?').join('.') + '$';
+      
+      // Create a regular expression object
+      const regex = new RegExp(regexPattern);
+      
+      // Test if the message matches the pattern
+      return regex.test(s);
+    };
+    
+    module.exports = decodeTheRing;
+    
+
   };
   
   module.exports = decodeTheRing;
